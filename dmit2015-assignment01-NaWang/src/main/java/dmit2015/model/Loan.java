17f8da3e291e28df2a1f650687f086c1fabdb3bb
dmit2015 <dmit2015@ubuntu>
2018-09-20 11:47:11 -0600

    update

--- dmit2015-assignment01-NaWang/src/main/java/dmit2015/model/Loan.java
+++ dmit2015-assignment01-NaWang/src/main/java/dmit2015/model/Loan.java
@@ -1,5 +1,59 @@
 package dmit2015.model;
 
 public class Loan {
+	
+	private double mortgageAmount;
+	private double annualInterestRate;
+	private int amortizationPeriod;
+	
+	public double getMortgageAmount() {
+		return mortgageAmount;
+	}
+	public void setMortgageAmount(double mortgageAmount) {
+		this.mortgageAmount = mortgageAmount;
+	}
+	public double getAnnualInterestRate() {
+		return annualInterestRate;
+	}
+	public void setAnnualInterestRate(double annualInterestRate) {
+		this.annualInterestRate = annualInterestRate;
+	}
+	public int getAmortizationPeriod() {
+		return amortizationPeriod;
+	}
+	public void setAmortizationPeriod(int amortizationPeriod) {
+		this.amortizationPeriod = amortizationPeriod;
+	}
+	public Loan() {
+		super();
+		// TODO Auto-generated constructor stub
+		this.mortgageAmount=mortgageAmount;
+		this.annualInterestRate=annualInterestRate;
+		this.amortizationPeriod=amortizationPeriod;
+		
+	}
+	public Loan(double mortgageAmount, double annualInterestRate, int amortizationPeriod) {
+		super();
+		this.mortgageAmount = mortgageAmount;
+		this.annualInterestRate = annualInterestRate;
+		this.amortizationPeriod = amortizationPeriod;
+	}
+//	@Override
+//	public String toString() {
+//		return "Loan [mortgageAmount=" + mortgageAmount + ", annualInterestRate=" + annualInterestRate
+//				+ ", amortizationPeriod=" + amortizationPeriod + "]";
+//	}
+//	
+	public double getMonthlyPayment()
+	{
+		
+	}
+	public LoanSchedule[] getLoanScheduleArray()
+	{
+		
+	}
+	
+	
+	
 
 }
