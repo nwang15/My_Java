85ba99ed66344587d0b96590c07decaafa0c6be4
dmit2015 <dmit2015@ubuntu>
2018-09-26 00:09:04 -0600

    update

---
+++ dmit2015-assignment01-NaWang/src/test/java/dmit2015/model/LoanTest.java
@@ -0,0 +1,14 @@
+package dmit2015.model;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class LoanTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
