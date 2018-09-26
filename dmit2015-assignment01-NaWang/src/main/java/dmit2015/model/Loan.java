c5c1b51528d36e62aa319ef8a17713cc63c87baf
dmit2015 <dmit2015@ubuntu>
2018-09-26 00:40:27 -0600

    formula update and unit test update

--- dmit2015-assignment01-NaWang/src/main/java/dmit2015/model/Loan.java
+++ dmit2015-assignment01-NaWang/src/main/java/dmit2015/model/Loan.java
@@ -6,54 +6,22 @@ public class Loan {
 	private double annualInterestRate;
 	private int amortizationPeriod;
 	
-	public double getMortgageAmount() {
-		return mortgageAmount;
-	}
-	public void setMortgageAmount(double mortgageAmount) {
-		this.mortgageAmount = mortgageAmount;
-	}
-	public double getAnnualInterestRate() {
-		return annualInterestRate;
-	}
-	public void setAnnualInterestRate(double annualInterestRate) {
-		this.annualInterestRate = annualInterestRate;
-	}
-	public int getAmortizationPeriod() {
-		return amortizationPeriod;
-	}
-	public void setAmortizationPeriod(int amortizationPeriod) {
-		this.amortizationPeriod = amortizationPeriod;
-	}
-	public Loan() {
-		super();
-		// TODO Auto-generated constructor stub
-		this.mortgageAmount=mortgageAmount;
-		this.annualInterestRate=annualInterestRate;
-		this.amortizationPeriod=amortizationPeriod;
+	public Loan()
+	{
 		
 	}
+	
 	public Loan(double mortgageAmount, double annualInterestRate, int amortizationPeriod) {
 		super();
 		this.mortgageAmount = mortgageAmount;
 		this.annualInterestRate = annualInterestRate;
 		this.amortizationPeriod = amortizationPeriod;
 	}
-//	@Override
-//	public String toString() {
-//		return "Loan [mortgageAmount=" + mortgageAmount + ", annualInterestRate=" + annualInterestRate
-//				+ ", amortizationPeriod=" + amortizationPeriod + "]";
-//	}
-//	
+	
 	public double getMonthlyPayment()
 	{
-		return mortgageAmount*(Math.pow(((1+annualInterestRate)/200),1.0/6.0)-1)/(1-Math.pow(Math.pow(((1+annualInterestRate)/200),1.0/6.0)+1, -12*amortizationPeriod));
-	}
-	
-	
-
-	public double getInterestPaid() 
-	{
-		return monthlyPercentageRate() * mortgageAmount;
+		return mortgageAmount*(Math.pow((1+annualInterestRate/200),(1.0/6.0))-1)
+				/(1-Math.pow(Math.pow((1+annualInterestRate/200),(1.0/6.0)), (-12*amortizationPeriod)));
 	}
 	
 	public LoanSchedule[] getLoanScheduleArray()
@@ -94,12 +62,58 @@ public class Loan {
 		
 		return loanScheduleTable;
 	}
+	
+	public double monthlyPercentageRate()
+	{
+		return Math.pow(((1+annualInterestRate)/200),1.0/6.0)-1;
+	}
+	
+	
+	public double getMortgageAmount() {
+		return mortgageAmount;
+	}
+	public void setMortgageAmount(double mortgageAmount) {
+		this.mortgageAmount = mortgageAmount;
+	}
+	public double getAnnualInterestRate() {
+		return annualInterestRate;
+	}
+	public void setAnnualInterestRate(double annualInterestRate) {
+		this.annualInterestRate = annualInterestRate;
+	}
+	public int getAmortizationPeriod() {
+		return amortizationPeriod;
+	}
+	public void setAmortizationPeriod(int amortizationPeriod) {
+		this.amortizationPeriod = amortizationPeriod;
+	}
+//	public Loan() {
+//		super();
+//		// TODO Auto-generated constructor stub
+//		this.mortgageAmount=mortgageAmount;
+//		this.annualInterestRate=annualInterestRate;
+//		this.amortizationPeriod=amortizationPeriod;
+//		
+//	}
+
+//	@Override
+//	public String toString() {
+//		return "Loan [mortgageAmount=" + mortgageAmount + ", annualInterestRate=" + annualInterestRate
+//				+ ", amortizationPeriod=" + amortizationPeriod + "]";
+//	}
+//	
+	
+	
+	
+
+	public double getInterestPaid() 
+	{
+		return monthlyPercentageRate() * mortgageAmount;
+	}
+	
+	
 				
 		
-		public double monthlyPercentageRate()
-		{
-			return Math.pow(((1+annualInterestRate)/200),1.0/6.0)-1;
-		}
 		
 		public double getPrincipalPaid()
 		{
