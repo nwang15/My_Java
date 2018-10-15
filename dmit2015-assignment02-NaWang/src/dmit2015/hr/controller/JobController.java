d774dee82e7d03b0b3fea433f2469c880da6c8c9
dmit2015 <dmit2015@ubuntu>
2018-10-14 19:41:38 -0600

    update

--- dmit2015-assignment02-NaWang/src/dmit2015/hr/controller/JobController.java
+++ dmit2015-assignment02-NaWang/src/dmit2015/hr/controller/JobController.java
@@ -4,13 +4,14 @@ import java.io.Serializable;
 import java.util.List;
 
 import javax.annotation.PostConstruct;
+import javax.ejb.EJBAccessException;
 import javax.faces.view.ViewScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
 
 import org.omnifaces.util.Messages;
 
-import com.sun.mail.handlers.message_rfc822;
+
 
 import dmit2015.hr.entity.Job;
 import dmit2015.hr.service.HumanResourceService;
@@ -18,7 +19,6 @@ import dmit2015.hr.service.HumanResourceService;
 
 @Named("currentJobController")
 @ViewScoped	
-
 public class JobController implements Serializable {
 	
 	private static final long serialVersionUID = 1L;
@@ -29,13 +29,40 @@ public class JobController implements Serializable {
 	private Job jobDetail;
 	private String Jobid;
 	private boolean editMode = false;
-	private String selectJobId;
+	private Integer selectJobId;
 	
 	@PostConstruct 
 	public void init() {
 		jobDetail = new Job();
 	}
 	
+	//create job
+	public String createJob()
+	{
+		String outcome = null;
+		
+		try
+		{
+			if(jobDetail.getMaxSalary().doubleValue() > 1.25 * jobDetail.getMinSalary().doubleValue())
+			{
+				humanResourceService.addJob(jobDetail);
+				jobDetail = new Job();
+				Messages.addFlashGlobalInfo("Create was successful.");
+				outcome = "viewJobs?faces-redirect=true";
+			}
+			
+		} catch(EJBAccessException e)
+		{
+			Messages.addGlobalError(e.getMessage());			
+		}catch(Exception e)
+		{
+			Messages.addGlobalError("Create was not successful.");
+		}
+		
+		return outcome;
+	}
+	
+	// find job by id
 	public void findJobById ()
 	{
 		if(Jobid !=null && Jobid.length()>0)
@@ -56,8 +83,63 @@ public class JobController implements Serializable {
 	}
 	
 	
+	// update job
+	public String updateJob()
+	{
+		String outcome = null;
+		
+		try
+		{
+			if(jobDetail.getMaxSalary().doubleValue() > 
+			1.25 * jobDetail.getMinSalary().doubleValue())
+			{
+				humanResourceService.updateJob(jobDetail);
+				jobDetail = new Job();
+				Messages.addFlashGlobalInfo("Update was successful.");
+				outcome = "viewJobs?faces-redirect=true";
+			}
+		}catch(EJBAccessException e)
+		{
+			Messages.addGlobalError(e.getMessage());			
+		}catch(Exception e)
+		{
+			Messages.addGlobalError("Update was not successful.");
+		}
+		
+		return outcome;
+	}
 	
+	// remove job
+	public String removeJob()
+	{
+		String outcome = null;
+		try {
+			humanResourceService.deleteJob(jobDetail);
+			Messages.addFlashGlobalInfo
+			("Remove Job {0} was successful", jobDetail.getJobId());
+			jobDetail = new Job();
+			editMode = false;
+			outcome = "viewJobs?faces-redirect=true";
+		}catch(EJBAccessException e)
+		{
+			Messages.addGlobalError(e.getMessage());			
+		}catch(Exception e)
+		{
+			Messages.addGlobalError("Remove was not successful.");
+		}
+		
+		return outcome;
+		
+	}
+	public String cancel() {
+		editMode = false;
+		return "viewJobs?faces-redirect=true";
+	}
 	
+	public List<Job> retreiveAllJobs() {
+		return humanResourceService.findAllJob();
+	}
+	// getter and setter
 	public Job getJobDetail() {
 		return jobDetail;
 	}
@@ -76,12 +158,14 @@ public class JobController implements Serializable {
 	public void setEditMode(boolean editMode) {
 		this.editMode = editMode;
 	}
-	public String getSelectJobId() {
+
+	public Integer getSelectJobId() {
 		return selectJobId;
 	}
-	public void setSelectJobId(String selectJobId) {
+
+	public void setSelectJobId(Integer selectJobId) {
 		this.selectJobId = selectJobId;
 	}
-	
+
 
 }
