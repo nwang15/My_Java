1fa57ad989dcc53b11f1cbe1572be7648b0b5115
dmit2015 <dmit2015@ubuntu>
2018-11-08 16:27:27 -0700

    update

---
+++ dmit2015-assignment02-NaWang/src/security/config/ApplicationConfig.java
@@ -0,0 +1,35 @@
+package security.config;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.security.enterprise.authentication.mechanism.http.CustomFormAuthenticationMechanismDefinition;
+import javax.security.enterprise.authentication.mechanism.http.LoginToContinue;
+import javax.security.enterprise.identitystore.LdapIdentityStoreDefinition;
+
+
+
+
+@CustomFormAuthenticationMechanismDefinition
+(
+		loginToContinue = @LoginToContinue(
+				loginPage="/security/customLogin.xhtml", 
+				errorPage="/security/customLogin.xhtml?error")
+)
+
+
+
+@LdapIdentityStoreDefinition(
+url = "ldap://metro-ds1.nait.ca:389",
+callerSearchBase = "dc=nait,dc=ca",
+callerNameAttribute = "SamAccountName",	
+groupSearchBase = "dc=nait,dc=ca",
+bindDn = "cn=DMIT Student1,ou=DMITStudentRestricted,ou=Student,ou=DMIT,ou=SICET,dc=nait,dc=ca",
+bindDnPassword = "Password2015",
+priority = 5
+)
+
+
+
+@ApplicationScoped
+public class ApplicationConfig {
+
+}
